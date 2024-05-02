d936532aa3a2200c6aa5fe84aa621aae48092479
RaphaelFrey <raphael.marius.frey@gmail.com>
2024-05-02 14:06:46 +0200

    Add additional auditing functionality

---
+++ book-network/src/main/java/com/rfrey/book/config/ApplicationAuditAware.java
@@ -0,0 +1,21 @@
+package com.rfrey.book.config;
+
+
+import com.rfrey.book.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            return Optional.empty();
+        }
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
