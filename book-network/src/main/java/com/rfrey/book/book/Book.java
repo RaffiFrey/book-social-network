aca389190fef2a894a64644501d33564a1db1b56
RaphaelFrey <raphael.marius.frey@gmail.com>
2024-05-02 07:16:36 +0200

    Added initial entities

---
+++ book-network/src/main/java/com/rfrey/book/book/Book.java
@@ -0,0 +1,44 @@
+package com.rfrey.book.book;
+
+
+import com.rfrey.book.common.BaseEntity;
+import com.rfrey.book.feedback.Feedback;
+import com.rfrey.book.history.BookTransactionHistory;
+import com.rfrey.book.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean sharable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+}
